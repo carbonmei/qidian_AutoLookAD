3cce6f71cc3b65323ade07a9d8e6ce659a344db3
carbonmei <34277424+carbonmei@users.noreply.github.com>
2023-06-06 22:22:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ MyClick("福利中心")
 sleep(2000)
 
 //看视频1-8
-for (let index = 1; index < 11; index++) {
+for (let index = 1; index < 12; index++) {
     let caption = "第" + parseInt(index) +"个"   
     var meButton = undefined
     if(index>8){
@@ -71,7 +71,7 @@ for (let index = 1; index < 11; index++) {
     }    
     meButton.click()
     toastLog("看视频等待20s" + caption)
-    sleep(20000)
+    sleep(21000)
     Close()  
     sleep(2000)
     toastLog("查找开心收下")
